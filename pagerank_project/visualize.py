94857deb2aa19d710e040b2d8138f51b6056f638
Oleksandr Lykhanskyi <alex.lykhanskyi@gmail.com>
2024-12-01 18:28:57 +0200

    Readme changes

--- pagerank_project/visualize.py
+++ pagerank_project/visualize.py
@@ -24,7 +24,7 @@ def graph_create(filename_results: str, filename_games: str) -> None:
     ranks = [value * 250 if value != 0.0 else 1200 for value in results.values()]
     colors = ["#66ff66", "#ffccff", "#ffcc99", "#cce5ff", "#ffff99", "#00af3a", "#ffbb30", "#d41b6b"]
     colors_to_choose = [choice(colors) for i in range(len(teams))]
-
+    
     G = nx.DiGraph()
     G.add_nodes_from(results.keys())
 
@@ -44,7 +44,7 @@ def graph_create(filename_results: str, filename_games: str) -> None:
 
     G.add_edges_from(games)
 
-    nx.draw(G, pos=nx.circular_layout(G), arrows = True, arrowsize = 15, node_color=colors_to_choose, node_size=ranks, with_labels=True, labels=node_labels, font_family="Times New Roman", font_color='black', font_size = 11)
+    nx.draw(G, pos=nx.circular_layout(G), arrows = True, arrowsize = 15, node_color=colors_to_choose, node_size=ranks, with_labels=True, labels=node_labels, font_family="Times New Roman", font_color='black', font_size = 11, font_weight = 'bold')
 
     plt.margins(0.1)
     plt.show()
