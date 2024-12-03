16fa9c0c5d8d6759e1d7343443da173571a3b853
Oleksandr Lykhanskyi <alex.lykhanskyi@gmail.com>
2024-12-03 22:48:17 +0200

    Minor changes after final review

--- pagerank_project/visualize.py
+++ pagerank_project/visualize.py
@@ -21,12 +21,12 @@ def graph_create(filename_results: str, filename_games: str) -> None:
         results = {el.strip().split(',')[0]: float(el.strip().split(',')[3]) for el in content}
 
     teams = results.keys()
-    ranks = [value * 250 if value != 0.0 else 1200 for value in results.values()]
+    ranks = [value * 300 for value in results.values()]
     colors = ["#66ff66", "#ffccff", "#ffcc99", "#cce5ff", "#ffff99", "#00af3a", "#ffbb30", "#d41b6b"]
     colors_to_choose = [choice(colors) for i in range(len(teams))]
     
     G = nx.DiGraph()
-    G.add_nodes_from(results.keys())
+    G.add_nodes_from(teams)
 
     for team, rank in results.items():
         G.nodes[team]['rank'] = rank
@@ -41,7 +41,7 @@ def graph_create(filename_results: str, filename_games: str) -> None:
                 content.append(','.join([el.strip().split(',')[1], el.strip().split(',')[0]]))
                     
     games = [(el.strip().split(',')[0], el.strip().split(',')[1]) for el in content]
-
+    
     G.add_edges_from(games)
 
     nx.draw(G, pos=nx.circular_layout(G), arrows = True, arrowsize = 15, node_color=colors_to_choose, node_size=ranks, with_labels=True, labels=node_labels, font_family="Times New Roman", font_color='black', font_size = 11, font_weight = 'bold')
