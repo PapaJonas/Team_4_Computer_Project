140cf641a63ea17d4a3c783f947fbe0794351799
daryna prytula <darynkaprytula11@gmail.com>
2024-12-03 13:05:55 +0200

    changes in documentation

--- pagerank_project/pagerank_write.py
+++ pagerank_project/pagerank_write.py
@@ -1,36 +1,42 @@
-'''A module of wruting function'''
+'''A module of writing function'''
 def write_function(filename, information, rank):
     """
     Records the ranking of the teams to a CSV file.
-    The function accepts a filename and a list of dictionaries,
-    where each dictionary represents a team.
-    Each dictionary contains:
-        - A single key: the team name (string).
-        - A value: a list with two elements (number of points and rank of the team).
-    The function sorts the teams by points in descending order and 
-    writes the sorted data to a CSV file
-    Parameters:
-        filename : str, the path of the CSV file where the data will be written.
-        information dict, dictionary containing team information.
-        rank (list[int]): List of ranks for the teams.
 
-    Return:
+    Parameters:
+        filename (str): The path of the CSV file where the data will be written.
+        information (dict): Dictionary containing team data:
+            - keys: Team names (strings).
+            - values: A list with three elements:
+                1. Number of points (int),
+                2. Placeholder rank (int, initially set to 0),
+                3. Number of games played (int).
+        rank (list[float]): Numerical values representing the teams' ranks.
+
+    Returns:
         None
 
     Example Usage:
     --------------
-    rank = [15, 10, 5]
-    information = {"team1": [99, 200, 2], "team2": [50, 50, 2]}, "team3": [50, 40, 2]}
+    rank = [29.47, 16.78, 14.93]
+    information = {
+        "team1": [5, 0, 3],
+        "team2": [4, 0, 3],
+        "team3": [4, 0, 3],
+    }
     write_function("example.csv", information, rank)
     """
     ans = []
     for i, team_data in enumerate(information):
         ans.append([information[team_data][0], rank[i], information[team_data][2], team_data])
+
     def func(ans):
         return ans[0:1]
     sorted_information = sorted(ans, key = func, reverse = True)
+
     with open(filename, 'w', encoding='utf-8') as file:
         file.write("team,games,points,rank\n")
+
         for team_data in sorted_information:
             points, rank_value, games, team_name = team_data
             file.write(f"{team_name},{games},{points},{rank_value}\n")
