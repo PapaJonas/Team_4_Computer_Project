be685c4c1a252835008040c110c1712a59568b99
Solchanyk Vasyl Hryhorovych <solchanykv@gmail.com>
2024-12-04 12:50:17 +0200

    last_commit

--- pagerank_project/pagerank_write.py
+++ pagerank_project/pagerank_write.py
@@ -31,7 +31,8 @@ def write_function(filename, information, rank):
         ans.append([information[team_data][0], rank[i], information[team_data][2], team_data])
 
     def func(ans):
-        return ans[0:1]
+        return ans[0], ans[1]
+    print(ans)
     sorted_information = sorted(ans, key = func, reverse = True)
 
     with open(filename, 'w', encoding='utf-8') as file:
